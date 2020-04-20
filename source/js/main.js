53f33429a834de7532069dbc11809e64d359f49c
GogeH <skibadee@inbox.ru>
2020-04-20 21:47:25 +0300

    footer, header, блок availability

--- source/js/main.js
+++ source/js/main.js
@@ -5,13 +5,9 @@
   var navToggle = document.querySelector('.main-nav__toggle');
 
   navMain.classList.remove('main-nav--nojs');
+  navToggle.classList.remove('main-nav__toggle--nojs');
 
-  navToggle.addEventListener('click', function() {
-    if (navMain.classList.contains('main-nav--closed')) {
-      navMain.classList.remove('main-nav--closed');
-      navMain.classList.add('main-nav--opened');
-    } else {
-      navMain.classList.add('main-nav--closed');
-      navMain.classList.remove('main-nav--opened');
-    }
+  navToggle.addEventListener('click', function () {
+    navMain.classList.toggle('main-nav--opened');
+    navToggle.classList.toggle('main-nav__toggle--opened');
   });
