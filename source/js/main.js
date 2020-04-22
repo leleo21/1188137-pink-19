0a4f38970101acc57ac40aab76b68f751ac77bdb
GogeH <skibadee@inbox.ru>
2020-04-22 21:38:18 +0300

    footer, header, index.html

--- source/js/main.js
+++ source/js/main.js
@@ -2,12 +2,12 @@
 
 
   var navMain = document.querySelector('.main-nav');
-  var navToggle = document.querySelector('.main-nav__toggle');
+  var navToggle = document.querySelector('.header__toggle');
 
   navMain.classList.remove('main-nav--nojs');
-  navToggle.classList.remove('main-nav__toggle--nojs');
+  navToggle.classList.remove('header__toggle--nojs');
 
   navToggle.addEventListener('click', function () {
     navMain.classList.toggle('main-nav--opened');
-    navToggle.classList.toggle('main-nav__toggle--opened');
+    navToggle.classList.toggle('header__toggle--opened');
   });
