e6030a190db8403e61a4c0d1765404f79690bd88
GogeH <skibadee@inbox.ru>
2020-05-06 23:33:04 +0300

    Погружение в автоматизацию(8 Задание)

--- source/js/main.js
+++ source/js/main.js
@@ -1,13 +1,12 @@
 'use strict';
 
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.header__toggle');
 
-  var navMain = document.querySelector('.main-nav');
-  var navToggle = document.querySelector('.header__toggle');
+navMain.classList.remove('main-nav--nojs');
+navToggle.classList.remove('header__toggle--nojs');
 
-  navMain.classList.remove('main-nav--nojs');
-  navToggle.classList.remove('header__toggle--nojs');
-
-  navToggle.addEventListener('click', function () {
-    navMain.classList.toggle('main-nav--opened');
-    navToggle.classList.toggle('header__toggle--opened');
-  });
+navToggle.addEventListener('click', function () {
+  navMain.classList.toggle('main-nav--opened');
+  navToggle.classList.toggle('header__toggle--opened');
+});
